164cab8cd6559f1cfa779d7bc72e46222606f05e
Aleksandra Krzemińska <krzeminska89@gmail.com>
2021-03-02 19:17:36 +0100

    Initial commit

---
+++ src/main/java/pl/sdacademy/podstawy/loading/LoadingExample.java
@@ -0,0 +1,32 @@
+package pl.sdacademy.podstawy.loading;
+
+public class LoadingExample {
+    public static final int STATIC_FIELD = Main.staticField();
+    public static final int STATIC_FIELD_INITIALIZER;
+
+    static {
+        STATIC_FIELD_INITIALIZER = Main.staticInitializer();
+    }
+
+    private final int objectField = Main.objectField();
+
+    private final int objectInitializer;
+
+    {
+        objectInitializer = Main.objectInitializer();
+
+    }
+    //private  int firstConstructor;
+    private final int secondConstructor;
+
+    public LoadingExample() {
+        this (1);
+        int firstConstructor = Main.firstConstructor();
+    }
+
+    public LoadingExample (int x) {
+       secondConstructor = Main.secondConstructor();
+    }
+
+}
+
